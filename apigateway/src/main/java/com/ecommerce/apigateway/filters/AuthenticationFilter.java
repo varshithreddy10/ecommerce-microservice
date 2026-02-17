4358ca5b0fb31c5796867118e6cde5ab96bd784b
N VARSHITH KUMAR REDDY <varshithreddy1009@gmail.com>
2026-02-17 07:52:54 +0530

    Update AuthenticationFilter.java

--- apigateway/src/main/java/com/ecommerce/apigateway/filters/AuthenticationFilter.java
+++ apigateway/src/main/java/com/ecommerce/apigateway/filters/AuthenticationFilter.java
@@ -31,6 +31,16 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
         log.info("PORTMAN control entered the AuthenticationFilter ");
         return (exchange, chain) -> {
 
+            if (path.contains("/v3/api-docs") ||
+                path.contains("/swagger-ui") ||
+                path.contains("/swagger-ui.html") ||
+                path.contains("/webjars") ||
+                path.contains("/api/auth")) {
+            
+                return chain.filter(exchange);
+            }
+
+
             String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
 
             log.info("NATALIE jwttoken is = "+authHeader);
